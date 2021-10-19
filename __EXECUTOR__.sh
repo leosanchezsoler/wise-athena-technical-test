e1be7b9967e67292398796ec23161bbbc24f6ac1
Leonardo Sánchez <74706379+leosanchezsoler@users.noreply.github.com>
2021-10-19 12:46:23 +0200

    last commit

--- __EXECUTOR__.sh
+++ __EXECUTOR__.sh
@@ -4,7 +4,7 @@
 
 # https://discuss.streamlit.io/t/problem-deploying-streamlit-app-with-docker/1644/4
 
-DOCKER_BUILD="ml_project_image:latest"
+DOCKER_BUILD="wise_athena_tech_test:latest"
 DOCKER_ID=$(docker ps -a -q --filter ancestor=${DOCKER_BUILD} --format="{{.ID}}")
 sudo docker rm $(docker stop $(docker ps -a -q --filter ancestor=${DOCKER_BUILD} --format="{{.ID}}"))
 
@@ -26,10 +26,9 @@ sudo docker exec ${DOCKER_ID} sh -c "echo The local IP is:"
 sudo docker exec ${DOCKER_ID} sh -c "hostname -I"
 
 #sudo docker exec ${DOCKER_ID} sh -c "python3 ./src/api/server.py"
-sudo docker exec ${DOCKER_ID} sh -c "nohup python3 ./src/api/server.py > server.log &"
 
-#sudo docker exec ${DOCKER_ID} sh -c "streamlit run ./src/dashboard/app.py"
-#sudo docker exec ${DOCKER_ID} sh -c "nohup streamlit run ./src/dashboard/app.py > app.log &"
+sudo docker exec ${DOCKER_ID} sh -c "streamlit run ./src/dashboard/app.py"
+sudo docker exec ${DOCKER_ID} sh -c "nohup streamlit run ./src/dashboard/app.py > app.log &"
 
 # sudo docker exec ml_project_image:latest sh -c "streamlit run ./src/dashboard/app.py"
-#sudo docker exec ${DOCKER_BUILD} sh -c "streamlit run ./src/dashboard/app.py"
+sudo docker exec ${DOCKER_BUILD} sh -c "streamlit run ./src/dashboard/app.py"
